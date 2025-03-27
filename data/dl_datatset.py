3182893e2cbd47742f04c453b9d95ddaf1a4204c
tch0905 <a1182116615@gmail.com>
2025-03-27 13:34:34 +0800

    add local dataset for GPU server usage

--- data/dl_datatset.py
+++ data/dl_datatset.py
@@ -1,4 +1,8 @@
 from datasets import load_dataset
 
-dataset = load_dataset("conll2003", trust_remote_code=True)
+# Download the dataset
+dataset = load_dataset("conll2003",trust_remote_code=True)
 
+# Save locally in a directory
+dataset.save_to_disk("../conll2003_local")
+print("Dataset downloaded and saved locally at './conll2003_local'")
