ac566af1f67be58d73b5fcf9c092cceee9196a53
tch0905 <a1182116615@gmail.com>
2025-03-28 14:48:23 +0800

    pretrain

--- utils/download_pretian_model.py
+++ utils/download_pretian_model.py
@@ -1,10 +1,12 @@
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, BertForTokenClassification
 
 # Download and save the tokenizer locally
 tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+model = BertForTokenClassification.from_pretrained("bert-base-cased", num_labels=9)
 
 # Save the tokenizer for offline usage
 tokenizer.save_pretrained("../bert-base-cased-local")
+model.save_pretrained("../bert-base-cased-local")
 print("Tokenizer downloaded and saved locally at '../bert-base-cased-local'")
 
 # Load the tokenizer from the local directory
