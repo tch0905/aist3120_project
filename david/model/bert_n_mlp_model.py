28d7d02f11f994007ac0b50de5cf29e09e02397a
tch0905 <a1182116615@gmail.com>
2025-04-24 00:36:53 +0800

    data aru

--- david/model/bert_n_mlp_model.py
+++ david/model/bert_n_mlp_model.py
@@ -103,7 +103,7 @@ training_args = TrainingArguments(
     output_dir="./",
     per_device_train_batch_size=BATCH_SIZE,
     per_device_eval_batch_size=BATCH_SIZE,
-    num_train_epochs=25,
+    num_train_epochs=8,
     learning_rate=5e-5,
     weight_decay=0.01,
     evaluation_strategy="epoch",
@@ -126,7 +126,7 @@ training_args = TrainingArguments(
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=tokenized_datasets_conll,
+    train_dataset=tokenized_datasets_wikiann['train'],
     eval_dataset=tokenized_datasets_conll_test,
     tokenizer=tokenizer,
     data_collator=data_collator,
@@ -135,34 +135,34 @@ trainer = Trainer(
 trainer.train()
 
 
-# results = trainer.evaluate(tokenized_datasets_conll_test)
-# print("Test Result:")
-# print(results)
-#
+results = trainer.evaluate(tokenized_datasets_conll_test)
+print("Test Result:")
+print(results)
+
 # trainer.save_model("./best_model")
 # print("=== Now training on conll ===")
 # training_args.num_train_epochs = 25  # Update to 25 epochs for CoNLL
 #
 # # Create a new trainer for CoNLL
-# trainer = Trainer(
-#     model=model,
-#     args=training_args,
-#     train_dataset=tokenized_datasets_conll,  # Use CoNLL training dataset
-#     eval_dataset=tokenized_datasets_conll_test,
-#     tokenizer=tokenizer,
-#     data_collator=data_collator,
-#     compute_metrics=compute_metrics
-# )
-#
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_datasets_conll,  # Use CoNLL training dataset
+    eval_dataset=tokenized_datasets_conll_test,
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics
+)
+
 #
 # state_dict = load_file(f"./best_model/model.safetensors")
 # model.load_state_dict(state_dict)
 
 # Update the trainer with the new model for the next dataset
-# trainer.train_dataset = tokenized_datasets_conll
-# trainer.eval_dataset = tokenized_datasets_conll_test
-# trainer.learning_rate = 2e-5
-# trainer.train()
+trainer.train_dataset = tokenized_datasets_conll
+trainer.eval_dataset = tokenized_datasets_conll_test
+trainer.learning_rate = 5e-5
+trainer.train()
 
 
 
